fe17c58850a1be72e8056cbf75a71138013df6d1
unknown <sanika2003.patil@gmail.com>
2025-07-28 21:19:06 +0530

    initial commit

---
+++ packages/app/src/types.ts
@@ -0,0 +1,43 @@
+// src/types/subscription.ts or src/types/api.ts
+
+export interface SubscriptionDataType {
+  price: number | string;
+  code: string;
+  tag: string;
+  currency: string;
+}
+
+export type ApiFailureType = {
+  status: "failure";
+  error: string;
+};
+
+export type GetSubscriptionsListApiResponse =
+  | {
+      status: "success";
+      data: SubscriptionDataType[];
+    }
+  | ApiFailureType;
+
+export type GetCurrentSubscriptionApiResponse =
+  | {
+      status: "success";
+      data: SubscriptionDataType;
+    }
+  | ApiFailureType;
+
+export type UpgradeSubscriptionApiResponse =
+  | {
+      status: "success";
+      subscription: SubscriptionDataType;
+      message: string;
+    }
+  | ApiFailureType;
+
+export type DowngradeSubscriptionApiResponse =
+  | {
+      status: "success";
+      subscription: SubscriptionDataType;
+      message: string;
+    }
+  | ApiFailureType;
